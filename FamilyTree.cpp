1284f1f5b8e18621509aeb1582f007da53c7b51f
shahareng <shaharbel0@gmail.com>
2020-04-06 15:39:24 +0300

    change files that ./demo and ./test was run

--- FamilyTree.cpp
+++ FamilyTree.cpp
@@ -9,9 +9,7 @@ namespace family
 
     Tree::Tree(string name)
     {
-        root->key_value = name;
-        root->left=NULL;
-        root->right=NULL;
+        // root->key_value = name;
     }
     
     // add father to someone that apear in the tree
@@ -31,26 +29,26 @@ namespace family
     // return the relation in the tree ; e.g: mothre, father, unrelated and more..
     string Tree::relation(string name)
     {
-
+        // throw an error if the name doesn't apear in the tree
         return "mother";
     }
 
     // return the name of the relation
     string Tree::find(string relat)
     {
-
+        // throw an error if relation doesn't exist
         return "Yosef";
     }
 
     // display the tree
     void Tree::display()
     {
-
+        cout << "hello" << endl;
     }
 
     // delete the name and all his parents from the tree
     void Tree::remove(string name)
     {
-
+        cout << "delete" << endl;
     }
 };
